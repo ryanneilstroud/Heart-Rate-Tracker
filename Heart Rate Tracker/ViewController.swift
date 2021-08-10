31f3058648144c700041ec44ce7be986e631e8db
Ryan Neil Stroud <ryanstroud75@gmail.com>
2021-08-10 10:39:25 +0800

    Initial Commit

---
+++ Heart Rate Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Heart Rate Tracker
+//
+//  Created by Ryan Neil Stroud on 10/8/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
